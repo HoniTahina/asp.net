1ab8242111dd9d5fce22eca11c233b7a17c0a300
HoniTahina <ricatahina@gmail.com>
2024-05-06 18:29:45 +0000

    integration de la maquette dans les projets mvc et webform

---
+++ Webform2024/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webform2024.Startup))]
+namespace Webform2024
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
