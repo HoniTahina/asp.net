e3e8a62fb4800b218a4c9433a990ca13c4d93df5
HoniTahina <ricatahina@gmail.com>
2024-06-10 20:48:25 +0000

    controller auteur

---
+++ MvcFoad2024/Controllers/AuteursController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcFoad2024.Models;
+
+namespace MvcFoad2024.Controllers
+{
+    public class AuteursController : Controller
+    {
+        private BdPartagememoireContext db = new BdPartagememoireContext();
+
+        // GET: Auteurs
+        public ActionResult Index()
+        {
+            return View(db.auteurs.ToList());
+        }
+
+        // GET: Auteurs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auteur auteur = db.auteurs.Find(id);
+            if (auteur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auteur);
+        }
+
+        // GET: Auteurs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Auteurs/Create
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdUtilisateur,Nom,Prenom,Telephone,Email,motDePasse,Matricule,Etat,Specialite,Anciennete")] Auteur auteur)
+        {
+            if (ModelState.IsValid)
+            {
+                db.auteurs.Add(auteur);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(auteur);
+        }
+
+        // GET: Auteurs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auteur auteur = db.auteurs.Find(id);
+            if (auteur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auteur);
+        }
+
+        // POST: Auteurs/Edit/5
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdUtilisateur,Nom,Prenom,Telephone,Email,motDePasse,Matricule,Etat,Specialite,Anciennete")] Auteur auteur)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(auteur).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(auteur);
+        }
+
+        // GET: Auteurs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auteur auteur = db.auteurs.Find(id);
+            if (auteur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auteur);
+        }
+
+        // POST: Auteurs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Auteur auteur = db.auteurs.Find(id);
+            db.auteurs.Remove(auteur);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcFoad2024/Controllers/DocumentsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcFoad2024.Models;
+
+namespace MvcFoad2024.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        private BdPartagememoireContext db = new BdPartagememoireContext();
+
+        // GET: Documents
+        public ActionResult Index()
+        {
+            var documents = db.documents.Include(d => d.Memoire);
+            return View(documents.ToList());
+        }
+
+        // GET: Documents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Document document = db.documents.Find(id);
+            if (document == null)
+            {
+                return HttpNotFound();
+            }
+            return View(document);
+        }
+
+        // GET: Documents/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdMemoire = new SelectList(db.memoires, "IdMemoire", "Annee");
+            return View();
+        }
+
+        // POST: Documents/Create
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nom,Extension,IdMemoire")] Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                db.documents.Add(document);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdMemoire = new SelectList(db.memoires, "IdMemoire", "Annee", document.IdMemoire);
+            return View(document);
+        }
+
+        // GET: Documents/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Document document = db.documents.Find(id);
+            if (document == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdMemoire = new SelectList(db.memoires, "IdMemoire", "Annee", document.IdMemoire);
+            return View(document);
+        }
+
+        // POST: Documents/Edit/5
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nom,Extension,IdMemoire")] Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(document).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdMemoire = new SelectList(db.memoires, "IdMemoire", "Annee", document.IdMemoire);
+            return View(document);
+        }
+
+        // GET: Documents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Document document = db.documents.Find(id);
+            if (document == null)
+            {
+                return HttpNotFound();
+            }
+            return View(document);
+        }
+
+        // POST: Documents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Document document = db.documents.Find(id);
+            db.documents.Remove(document);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
